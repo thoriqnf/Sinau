8c5ce90c1de916ed8da7941ac60c4e03f326c3b9
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-07 15:24:06 +0700

    creating Login.js

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,6 +1,85 @@
 import React from "react";
+import { Row, Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { Link } from "react-router-dom";
 
-const Login = () => <h1>Login</h1>;
+// LOGO
+const Logo = () => (
+  <h1>
+    <img src="../img/sinau.png" />
+  </h1>
+);
 
+//HEADER
+
+const Tagline = () => <h2>TAGLINE AND DESCRIPTION</h2>;
+
+// COLUMN_SIGNUP
+
+const ColumnSignup = () => (
+  <Col xs={6}>
+    <h3>Sign Up</h3>
+    <Form>
+      <FormGroup>
+        <Label for="signupFullName">Full Name</Label>
+        <Input type="email" name="signupFullName" id="signupFullName" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="signupEmail">Email</Label>
+        <Input type="email" name="signupEmail" id="signupEmail" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="signupPassword">Password</Label>
+        <Input type="password" name="signupPassword" id="signupPassword" />
+      </FormGroup>
+      <Button>Sign Up</Button>
+    </Form>
+  </Col>
+);
+
+// COLUMN_LOGIN
+
+const ColumnLogin = () => (
+  <Col xs={6}>
+    <h3>Login</h3>
+    <Form>
+      <FormGroup>
+        <Label for="loginEmail">Email</Label>
+        <Input type="email" name="loginEmail" id="loginEmail" />
+      </FormGroup>
+      <FormGroup>
+        <Label for="loginPassword">Password</Label>
+        <Input type="password" name="loginPassword" id="loginPassword" />
+      </FormGroup>
+      <FormGroup>
+        <Link to="/forgot">Forgot password?</Link>
+      </FormGroup>
+      <Button>Login</Button>
+    </Form>
+  </Col>
+);
+
+// LOGIN
+
+const Login = () => (
+  <div>
+    <Row className="text-center" id="row-logo">
+      <Col>
+        <Logo />
+      </Col>
+    </Row>
+
+    <Row className="text-center" id="row-tagline">
+      <Col>
+        <Tagline />
+      </Col>
+    </Row>
+
+    <hr />
+
+    <Row className="center" id="row-auth">
+      <ColumnSignup />
+      <ColumnLogin />
+    </Row>
+  </div>
+);
 export default Login;
