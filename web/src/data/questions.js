b055cfe5aaaaa9a0fbd5351c032acd42666ed0f9
thoriqnf <thoriqnfaizal@gmail.com>
2017-11-08 11:09:53 +0700

    creating data users

---
+++ web/src/data/questions.js
@@ -0,0 +1,60 @@
+module.exports = [
+  {
+    _id: 1,
+    title: "Why do happy people always happy?",
+    createdBy: {
+      _id: 1,
+      name: "M Haidar Hanif"
+    },
+    answers: [
+      {
+        _id: 0,
+        text:
+          "The answer we have been looking for. Adaptogen sartorial sunt truffaut, four loko tote bag seitan fugiat sint commodo trust fund dolore blue bottle mollit hammock. Freegan culpa sartorial green juice leggings, snackwave palo santo enim. Street art four loko crucifix wolf, you probably haven't heard of them duis kale chips incididunt four dollar toast velit affogato labore af sriracha coloring book. ",
+        createdBy: {
+          _id: 0,
+          name: "Admin"
+        }
+      },
+      {
+        _id: 1,
+        text:
+          "Another cool answer we are waiting. Air plant helvetica officia, authentic tattooed vaporware af aute live-edge 90's. Jean shorts kale chips waistcoat, nostrud craft beer af palo santo meditation. Franzen +1 whatever edison bulb.",
+        createdBy: {
+          _id: 1,
+          name: "M Haidar Hanif"
+        }
+      }
+    ]
+  },
+
+  {
+    _id: 2,
+    title: "Why we need to become hungry?",
+    createdBy: {
+      _id: 2,
+      name: "Other User"
+    },
+    answers: [
+      {
+        _id: 1,
+        text:
+          "Some useful answers. Duis lumbersexual neutra salvia raw denim. Enim esse hashtag ramps neutra, excepteur art party humblebrag chicharrones meh laborum blue bottle austin dolore chia. Trust fund schlitz qui eu, dreamcatcher jianbing coloring book synth nisi. Qui excepteur jean shorts microdosing.",
+        createdBy: {
+          _id: 2,
+          name: "Other User"
+        }
+      }
+    ]
+  },
+
+  {
+    _id: 3,
+    title: "Why we are like to cooling off?",
+    createdBy: {
+      _id: 0,
+      name: "M Haidar Hanif"
+    },
+    answers: []
+  }
+];
---
+++ web/src/data/user.js
@@ -0,0 +1,72 @@
+module.exports = [
+  {
+    _id: 0,
+    name: "Admin",
+    title: "Administrator"
+  },
+  {
+    _id: 1,
+    name: "Thoriq1",
+    title: "TK"
+  },
+  {
+    _id: 2,
+    name: "Thoriq2",
+    title: "SD"
+  },
+  {
+    _id: 3,
+    name: "Thoriq3",
+    title: "SMP"
+  },
+  {
+    _id: 4,
+    name: "Thoriq4",
+    title: "SMA"
+  },
+  {
+    _id: 5,
+    name: "Thoriq5",
+    title: "SEMESTER1"
+  },
+  {
+    _id: 6,
+    name: "Thoriq6",
+    title: "SEMESTER2"
+  },
+  {
+    _id: 7,
+    name: "Thoriq7",
+    title: "SEMESTER3"
+  },
+  {
+    _id: 8,
+    name: "Thoriq8",
+    title: "SEMESTER4"
+  },
+  {
+    _id: 9,
+    name: "Thoriq9",
+    title: "SEMESTER5"
+  },
+  {
+    _id: 10,
+    name: "Thoriq10",
+    title: "SEMESTER6"
+  },
+  {
+    _id: 11,
+    name: "Thoriq11",
+    title: "SEMESTER7"
+  },
+  {
+    _id: 12,
+    name: "Thoriq12",
+    title: "SEMESTER8"
+  },
+  {
+    _id: 13,
+    name: "Thoriq13",
+    title: "S1"
+  }
+];
